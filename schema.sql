f53ea6a4fdc70ced02d17bb7867a1944014ff7fc
Adryndian <diancle01@gmail.com>
2026-02-25 22:55:11 +0800

    detel

--- schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS ugc_generations (
-  id TEXT PRIMARY KEY,
-  created_at TEXT DEFAULT (datetime('now')),
-  brand_name TEXT,
-  product_type TEXT,
-  input_brief TEXT, -- Stores JSON string of the input form data
-  output_plan TEXT, -- Stores JSON string of the generated result
-  user_id TEXT
-);
